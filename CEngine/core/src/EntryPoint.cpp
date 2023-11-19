6ccbcb63af20c828c4b03e2f4a0b9479f5ee635b
inogea <maziknewacc@gmail.com>
2023-11-19 22:09:51 +0300

    Final final commit

--- CEngine/core/src/EntryPoint.cpp
+++ CEngine/core/src/EntryPoint.cpp
@@ -19,21 +19,5 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 		int Result = app.run();																							
 		return Result;
 	}
-	//Обработка исключений
-	catch (const CubeException& e)
-	{
-		MessageBoxA(nullptr, e.whatEx(), e.getType(), MB_OK | MB_ICONEXCLAMATION);
-	}
-	catch (const std::exception& e)
-	{
-		CUBE_CORE_ERROR("Standart Exception was thrown-> \n{} Standart Exception", e.what());
-		MessageBoxA(nullptr, e.what(), "Standart Exception", MB_OK | MB_ICONEXCLAMATION);
-	}
-	catch (...)
-	{
-		CUBE_CORE_ERROR("Unknown Exception was thrown");
-		MessageBoxA(nullptr, "No details avalible", "Unknown Exception", MB_OK | MB_ICONEXCLAMATION);
-	}
-	return -1;
 }
 
