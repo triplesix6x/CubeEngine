4af99bad8a47bdc5c00d19b1abf966f190b4b133
inogea <maziknewacc@gmail.com>
2023-11-19 22:10:48 +0300

    final 4.0

--- CEngine/core/src/EntryPoint.cpp
+++ CEngine/core/src/EntryPoint.cpp
@@ -8,16 +8,13 @@
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	HRESULT hResult = CoInitialize(NULL);
-	try
-	{
-		//инициализация логирования
-		Cube::Log::init();
+	//инициализация логирования
+	Cube::Log::init();
 
-		//создание экземпляра приложения
-		Cube::Application app(GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN), Cube::WindowType::CUSTOM);	
+	//создание экземпляра приложения
+	Cube::Application app(GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN), Cube::WindowType::CUSTOM);	
 
-		int Result = app.run();																							
-		return Result;
-	}
+	int Result = app.run();																							
+	return Result;
 }
 
